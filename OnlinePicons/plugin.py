cbdd1915dc6abe3ff3f33d5a834613b38b86e940
Ali K. <59113007+dreamboxone@users.noreply.github.com>
2026-07-28 20:54:46 +0330

    Add files via upload

--- OnlinePicons/plugin.py
+++ OnlinePicons/plugin.py
@@ -155,7 +155,7 @@ TRANSLATIONS = {
         "Destination: %s": "مسیر: %s",
         "Checking download servers...": "در حال بررسی سرورهای دانلود...",
         "OK: Select/Unselect     ": "OK: انتخاب/لغو انتخاب     ",
-        ": Download": "Download",
+        ": Download": "Download :",
         "EXIT: Back": "EXIT: بازگشت",
         "Online": "آنلاین",
         "Backup server": "سرور پشتیبان",
@@ -561,7 +561,7 @@ class DestinationScreen(Screen):
                 backgroundColor="#202020" transparent="0" />
         <widget name="keysLeft" position="130,485" size="390,42"
                 font="Regular;22" halign="right" />
-        <widget name="greenKey" position="685,648" size="60,30"
+        <widget name="greenKey" position="710,648" size="60,30"
                 font="Regular;22" halign="center" foregroundColor="#00ff00" />
         <widget name="keysRight" position="598,485" size="270,42"
                 font="Regular;22" halign="left" />
@@ -692,7 +692,7 @@ class DownloadScreen(Screen):
                 font="Regular;22" halign="right" />
         <widget name="greenKey" position="550,648" size="60,30"
                 font="Regular;22" halign="center" foregroundColor="#00ff00" />
-        <widget name="downloadKey" position="550,648" size="135,30"
+        <widget name="downloadKey" position="550,648" size="160,30"
                 font="Regular;22" halign="left" />
         <widget name="exitKey" position="815,648" size="180,30"
                 font="Regular;22" halign="left" />
